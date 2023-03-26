46324d18ba67505fe720c03af26f20bfcf2d4456
Norbert B <dnorbertb@gmail.com>
2023-03-26 19:15:28 +0200

    Content changes

--- data/AboutContent.ts
+++ data/AboutContent.ts
@@ -5,15 +5,15 @@ export const pArr = [
     },
     {
         title: "About me technically",
-        content: "I am an IT technician by education, currently working as Junior Front-end developer for Riotters based in Szczecin, Poland. My adventure with IT began when I was 12 and since then I have learned a lot about different fields of IT. I serviced various electronic devices, like PC’s or mobile phones, configured networks, used various operating systems on VPS servers and PC computers. I have experience with Linux (Debian and Debian based, Manjaro, Arch), macOS and Windows.  I configured game servers, VPN servers, databases, hosted Node.js applications, using applications such as nginx and pm2. I did a lot of different things related to computer science that are simply too many to describe them all. I even coded controllers or worked with computer diagnostics of vehicles."
+        content: "I am an IT technician by education, currently working as Front-end developer for Riotters based in Szczecin, Poland. My adventure with IT began when I was 12 and since then I have learned a lot about different fields of IT. I serviced various electronic devices, like PC’s or mobile phones, configured networks, used various operating systems on VPS servers and PC computers. I have experience with Linux (Debian and Debian based, Manjaro, Arch), macOS and Windows.  I configured game servers, VPN servers, databases, hosted Node.js applications, using applications such as nginx and pm2. I did a lot of different things related to computer science that are simply too many to describe them all. I even coded controllers or worked with computer diagnostics of vehicles."
     },
     {
         title: "In the future",
-        content: "What I want to learn will mainly depend on what I will need at work, but I have already thought about it and I think I want to learn C# and Go. I also like creating mobile applications, I am currently working on one application in React Native but as I am an iPhone user, Swift need to definitely appear in my stack. I also plan to learn about Azure and other cloud related tools."
+        content: "What I want to learn will mainly depend on what I need at work, but I have already thought about it and I think I want to learn C# and Go. I also like creating mobile applications, I am currently working on one application in React Native but as I am an iPhone user, Swift need to definitely appear in my stack. I also plan to learn about Azure and other cloud related tools."
     },
-    {
-        title: "Comment",
-        content: "As I mentioned earlier, I have a lot of different knowledge, which allows me to quickly understand and learn new things. While learning web development, I wanted to get to know as many different tools as possible, I didn't focus on anything specific because I want to specialize in that I will need at work. I am currently able to work with popular frameworks such as React, Vue2, Vue3, Next or Nuxt and I also think that I will quickly get into Angular (something I have already done, I know the concepts) if necessary. I still have a lot to learn but with my learning speed and experienced people around me I will be progressing very quickly."
-    }
+    // {
+    //     title: "Comment",
+    //     content: "As I mentioned earlier, I have a lot of different knowledge, which allows me to quickly understand and learn new things. While learning web development, I wanted to get to know as many different tools as possible, I didn't focus on anything specific because I want to specialize in that I will need at work. I am currently able to work with popular frameworks such as React, Vue2, Vue3, Next or Nuxt and I also think that I will quickly get into Angular (something I have already done, I know the concepts) if necessary. I still have a lot to learn but with my learning speed and experienced people around me I will be progressing very quickly."
+    // }
 ]
 
