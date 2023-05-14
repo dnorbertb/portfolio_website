8a6deecc66e3fdb8106bd43f925c365bb8beaf07
Norbert B <dnorbertb@gmail.com>
2023-05-14 23:08:40 +0200

    Fixes for new Next.js version

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Image from 'next/image';
 import Icon from '../components/Icon';
 import { mdiLinkedin, mdiGithub, mdiStackOverflow } from '@mdi/js';
 import { Metadata } from 'next';
 
+import profilePic from '../assets/me.jpg';
 import styles from '../styles/Modules/HomePage.module.scss';
 
 export const metadata: Metadata = {
@@ -15,30 +17,32 @@ export default function Page() {
       <div className={styles.greeting}>
         <span className={styles['greeting__hi']}>Hi!</span>
         <h1 className={styles['greeting__primary']}>
-          I'm <strong>Norbert!</strong>
+          I&apos;m <strong>Norbert!</strong>
         </h1>
         <h2 className={styles['greeting__secondary']}>
-          I'm a &lt;developer&gt;!
+          I&apos;m a &lt;developer&gt;!
         </h2>
       </div>
-      <img
+      <Image
         className={styles['profile-image']}
-        src="/me.jpg"
+        src={profilePic}
         alt="Memoji of me"
       />
       <div className={styles.accounts}>
         <a
           href="https://www.linkedin.com/in/norbert-bednarczyk-931692227/"
           target="_blank"
+          rel="noreferrer"
         >
           <Icon className={styles['accounts__icon']} svgPath={mdiLinkedin} />
         </a>
-        <a href="https://github.com/dnorbertb" target="_blank">
+        <a href="https://github.com/dnorbertb" target="_blank" rel="noreferrer">
           <Icon className={styles['accounts__icon']} svgPath={mdiGithub} />
         </a>
         <a
           href="https://stackoverflow.com/users/17151859/norbert"
           target="_blank"
+          rel="noreferrer"
         >
           <Icon
             className={styles['accounts__icon']}
